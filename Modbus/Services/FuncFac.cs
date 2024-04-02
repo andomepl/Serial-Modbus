2db43bc0d56dc634b6fe62910e9e39a4f8009562
NagiTang Cheong <andomepl@gmail.com>
2024-04-02 14:56:26 +0800

    Button Add line dash array

--- Modbus/Services/FuncFac.cs
+++ Modbus/Services/FuncFac.cs
@@ -13,7 +13,40 @@ namespace ModbusTool.Services
 {
     public class FuncFac
     {
+        public static async Task MasterFunc01(IModbusMaster modbusMaster, byte slaveAddress, CancellationToken token, ObservableCollection<MasterTable> masterTables)
+        {
+            try
+            {
+                await Task.Run(async () =>
+                {
+                    bool[]? datas = null;
+                    while (true)
+                    {
+                        await Task.Delay(1000);
 
+                        if (token.IsCancellationRequested)
+                            token.ThrowIfCancellationRequested();
+
+                        datas = await modbusMaster.ReadCoilsAsync(slaveAddress, (ushort)0, (ushort)10);
+
+                        if (datas == null)
+                            continue;
+
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            for (int i = 0; i < 10; i++)
+                            {
+                                masterTables[i].Value =datas[i]?(ushort)1:(ushort)0;
+                            }
+                        });
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
         public static async Task MasterFunc03(IModbusMaster modbusMaster,byte slaveAddress,CancellationToken token,ObservableCollection<MasterTable> masterTables)
         {
             try
@@ -45,72 +78,6 @@ namespace ModbusTool.Services
                 }, token);
             }
             catch(Exception e)
-            {
-                throw;
-            }
-        }
-
-
-
-        public static async Task MasterFunc06(IModbusMaster modbusMaster, byte slaveAddress, CancellationToken token, ObservableCollection<MasterTable> masterTables)
-        {
-
-            try
-            {
-                await Task.Run(async () =>
-                {
-
-                    while (true)
-                    {
-                        await Task.Delay(1000);
-
-                        if (token.IsCancellationRequested)
-                            token.ThrowIfCancellationRequested();
-
-                        ushort[] us = masterTables.Select(ms => ms.Value).ToArray();
-
-                        await modbusMaster.WriteMultipleRegistersAsync(slaveAddress, 0, us);
-                    }
-                });
-            }
-            catch(Exception e)
-            {
-                throw;
-            }
-
-        }
-    
-    
-        public static async Task MasterFunc01(IModbusMaster modbusMaster, byte slaveAddress, CancellationToken token, ObservableCollection<MasterTable> masterTables)
-        {
-            try
-            {
-                await Task.Run(async () =>
-                {
-                    bool[]? datas = null;
-                    while (true)
-                    {
-                        await Task.Delay(1000);
-
-                        if (token.IsCancellationRequested)
-                            token.ThrowIfCancellationRequested();
-
-                        datas = await modbusMaster.ReadCoilsAsync(slaveAddress, (ushort)0, (ushort)10);
-
-                        if (datas == null)
-                            continue;
-
-                        Application.Current.Dispatcher.Invoke(() =>
-                        {
-                            for (int i = 0; i < 10; i++)
-                            {
-                                masterTables[i].Value =datas[i]?(ushort)1:(ushort)0;
-                            }
-                        });
-                    }
-                });
-            }
-            catch (Exception e)
             {
                 throw;
             }
@@ -151,5 +118,96 @@ namespace ModbusTool.Services
                 throw;
             }
         }
+        public static async Task MasterFunc06(IModbusMaster modbusMaster, byte slaveAddress, CancellationToken token, ObservableCollection<MasterTable> masterTables)
+        {
+
+            try
+            {
+                await Task.Run(async () =>
+                {
+
+                    while (true)
+                    {
+                        await Task.Delay(1000);
+
+                        if (token.IsCancellationRequested)
+                            token.ThrowIfCancellationRequested();
+
+                        ushort[] us = masterTables.Select(ms => ms.Value).ToArray();
+
+                        await modbusMaster.WriteMultipleRegistersAsync(slaveAddress, 0, us);
+                    }
+                });
+            }
+            catch(Exception e)
+            {
+                throw;
+            }
+
+        }
+
+
+        public static async Task MasterFunc15(IModbusMaster modbusMaster, byte slaveAddress, CancellationToken token, ObservableCollection<MasterTable> masterTables)
+        {
+            try
+            {
+                await Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        await Task.Delay(1000);
+
+                        if (token.IsCancellationRequested)
+                            token.ThrowIfCancellationRequested();
+
+                        bool[] us = masterTables.Select(ms =>
+                        {
+                            if (ms.Value == 1)
+                                return true;
+                            else
+                            {
+                                return false;
+                            }
+                        }).ToArray();
+
+                        await modbusMaster.WriteMultipleCoilsAsync(slaveAddress, (ushort)0, us);
+
+
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+        public static async Task MasterFunc16(IModbusMaster modbusMaster, byte slaveAddress, CancellationToken token, ObservableCollection<MasterTable> masterTables)
+        {
+
+            try
+            {
+                await Task.Run(async () =>
+                {
+
+                    while (true)
+                    {
+                        await Task.Delay(1000);
+
+                        if (token.IsCancellationRequested)
+                            token.ThrowIfCancellationRequested();
+
+                        ushort[] us = masterTables.Select(ms => ms.Value).ToArray();
+
+                        await modbusMaster.WriteMultipleRegistersAsync(slaveAddress, 0, us);
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+        }
+    
     }
 }
